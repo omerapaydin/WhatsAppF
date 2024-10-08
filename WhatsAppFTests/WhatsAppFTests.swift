642c970cde95cbb170f00f3cfe448bbd94977e79
Ömer Apaydın <omerapaydin90@gmail.com>
2024-10-08 22:34:19 +0300

    Initial Commit

---
+++ WhatsAppFTests/WhatsAppFTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppFTests.swift
+//  WhatsAppFTests
+//
+//  Created by Ömer on 8.10.2024.
+//
+
+import Testing
+@testable import WhatsAppF
+
+struct WhatsAppFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
