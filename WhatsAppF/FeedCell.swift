ab4dbf58be2e9b8c127a55f1a9d4faccaf6a0e15
Ömer Apaydın <omerapaydin90@gmail.com>
2024-10-09 01:15:04 +0300

    Tasarım tamamlandı

---
+++ WhatsAppF/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  WhatsAppF
+//
+//  Created by Ömer on 9.10.2024.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
